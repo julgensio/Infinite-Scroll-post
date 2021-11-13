1a450a8e1f62db9760422e08335326c7c7201d43
Crunk <jb10051075@gmail.com>
2021-11-13 21:24:31 +0100

    Added body post from api into the post container

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const postContainer = document.getElementById('post-container');
+log = console.log;
+const postContainer = document.getElementById('posts-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
 let limitNumber = 3;
 let pageNumber = 1;
 
+// ! Fetch post from API
 async function getPost() {
 	const res = await fetch(
 		`https://jsonplaceholder.typicode.com/posts?_limit=${limitNumber}&_page${pageNumber}`
@@ -12,3 +14,29 @@ async function getPost() {
 	const data = await res.json();
 	return data;
 }
+
+// ! Show post in DOM
+async function showPosts() {
+	const posts = await getPost();
+
+	// Get post one by one
+	posts.forEach((post) => {
+		// ! Create post container from api
+		const postEl = document.createElement('div');
+		postEl.classList.add('post');
+		postEl.innerHTML = `
+        <div class="number">${post.id}</div>
+        <div class="post-info">
+            <h2 class="title">${post.title}</h2>
+            <p class="post-body">${post.body}</p>
+        </div>`;
+
+		// ! Add the post data in to the post container
+		postContainer.appendChild(postEl);
+	});
+}
+
+// Show initial posts
+showPosts();
+
+window.addEventListener('scroll', () => {});
